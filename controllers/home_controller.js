b88500b0f94cc02a4f2f4d6621d04e5395411e89
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2023-11-02 14:26:17 +0530

    style home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -5,3 +5,8 @@ module.exports.home = function (req, res) {
   });
   //   return res.end("<h1>Home page has been set up</h1>");
 };
+
+module.exports.Add_task = function (req, res) {
+  console.log(req.body);
+  return res.redirect("/");
+};
--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,6 @@ const router = express.Router();
 const homeController = require("../controllers/home_controller");
 // console.log("Router is working");
 router.get("/", homeController.home);
+router.use(express.urlencoded());
+router.post("/create-task", homeController.Add_task);
 module.exports = router;

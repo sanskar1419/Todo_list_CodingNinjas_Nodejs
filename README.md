10f8f9c92b9df0c897b378903ef93dffe18ba383
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2023-11-03 16:30:37 +0530

    Add Readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# ToDo_App
+
+My first Node js project
+
+## My process
+
+To-Do application is based on NodeJs and express as server side technology, MongoDb for Data-Base,and ejs fro front-end.
+
+### Built with
+
+- HTML
+- CSS
+- JavaScript
+- AJAX
+- NodeJs
+- express as web framework
+- MongoDB as db
+- ejs as veiw engine
+- Mongoose
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,14 @@
 const mongoose = require("mongoose");
+// Connecting to Database
 mongoose.connect("mongodb://localhost/Todo_list_db");
 
+// Aquiring the connection
 const db = mongoose.connection;
+
+// If there is error print error
 db.on("error", console.error.bind(console, "Error in connecting to DB!!!!!!"));
+
+// If connection is working print this
 db.once("open", function () {
   console.log("Succesfully connected to Database!!!!!!!!!!!!!!!!");
 });
